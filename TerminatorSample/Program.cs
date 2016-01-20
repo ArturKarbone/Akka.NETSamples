188aa93f425cf8d846604579adfeeac6ae074adc
ArturKarbone <arturk@diatomenterprises.com>
2016-01-20 18:19:28 +0200

    Initial Commit

---
+++ TerminatorSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using System;
+
+namespace TerminatorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().Wait();
+        }
+
+        private static async System.Threading.Tasks.Task AsyncMain()
+        {
+            using (var myActorSystem = ActorSystem.Create("myActorSystem"))
+            {
+                var terminatorActor = myActorSystem.ActorOf(Props.Create(() => new TerminatorActor()), "terminatorActor");
+
+                //emulate unhandled message
+                terminatorActor.Tell("Terminate this system");
+
+                terminatorActor.Tell(new TerminatorActor.Terminate());
+
+                await myActorSystem.WhenTerminated;
+
+                Console.WriteLine("Actor System has been terminated");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TerminatorSample/TerminatorActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+
+namespace TerminatorSample
+{
+    class TerminatorActor : ReceiveActor
+    {
+        #region Messages
+
+        public class Terminate
+        {
+
+        }
+
+        #endregion
+
+        public TerminatorActor()
+        {   
+            this.Receive<Terminate>(m =>
+            {
+                Context.System.Terminate();
+            }, m =>
+            {
+                return true;
+                //Use Consesus pattern here to make sure that the sender can actually shutdown the system
+                return this.Sender.Path.ToString().Contains("SuperHero");
+                //return this.Sender.Path.ToString().Contains("akka://all-systems/");
+            });
+        }
+    }
+}
