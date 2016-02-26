680a991aea5b0bf2d616c21af2e400cf1d93d7d8
ArturKarbone <arturk@diatomenterprises.com>
2016-02-26 18:37:33 +0200

    GitHubAPI sample first draft

---
+++ GitHubAPISample/GitHubAPI.Tests/CommitCounterServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace GitHubAPI.Tests
+{
+    [TestClass]
+    public class CommitCounterServiceTest
+    {
+        [TestMethod]
+        public  async Task Test()
+        {
+
+        }
+
+        [TestMethod]
+        public async Task CountCommitsSimpleTest()
+        {
+            Debug.WriteLine("Test Started");
+            var sw = Stopwatch.StartNew();
+
+            var service = new CommitCounterService();
+            var commitsCount = await service.CountCommits();
+
+            sw.Stop();
+
+            Console.WriteLine("Time it took to get the data - " + sw.ElapsedMilliseconds);
+            Console.WriteLine(commitsCount);
+
+            Assert.AreEqual(8921, commitsCount);
+        }
+
+        [TestMethod]
+        public async Task CountCommitsOptimzedTest()
+        {
+            Debug.WriteLine("Test Started");
+            var sw = Stopwatch.StartNew();
+
+            var service = new CommitCounterService();
+            var commitsCount = await service.CountCommitsOptimized();
+
+            sw.Stop();
+
+            Console.WriteLine("Time it took to get the data - " + sw.ElapsedMilliseconds);
+            Console.WriteLine(commitsCount);
+
+            Assert.AreEqual(8921, commitsCount);
+        }
+    }
+}
---
+++ GitHubAPISample/GitHubAPI/CommitCounterService.cs
@@ -0,0 +1,54 @@
+﻿using Octokit;
+using Octokit.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubAPI
+{
+    public class CommitCounterService
+    {
+        const string GitHubToken = "SomeToken"; 
+
+        public async Task<int> CountCommits()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("AkkaCounter"), new InMemoryCredentialStore(new Credentials(GitHubToken)));
+
+            var repos = await github.Repository.GetAllForUser("michal-franc");
+
+            var countCommits = 0;
+
+            foreach (var repository in repos)
+            {
+                var commits = await github.Repository.Commits.GetAll(repository.Owner.Login, repository.Name);
+                countCommits = countCommits + commits.Count;
+            }
+
+            return countCommits;
+        }
+
+        public async Task<int> CountCommitsOptimized()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("AkkaCounter"), new InMemoryCredentialStore(new Credentials(GitHubToken)));
+
+            var repos = await github.Repository.GetAllForUser("michal-franc");
+
+            var countCommits = 0;
+
+            var commitsResults = repos
+                .Select(repo => (github.Repository.Commits.GetAll(repo.Owner.Login, repo.Name)))
+                .ToList();
+            //.ForEach(async commits => countCommits += (await commits).Count);     
+
+            foreach (var commitResult in commitsResults)
+            {
+                countCommits += (await commitResult).Count;
+            }
+
+
+            return countCommits;
+        }
+    }
+}
