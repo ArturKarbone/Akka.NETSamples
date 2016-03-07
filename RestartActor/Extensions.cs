30d221d457e3426b7a4ac22129ef8da2bd76eddd
ArturKarbone <arturk@diatomenterprises.com>
2016-03-07 19:17:11 +0200

    An actor which can accept Restart message and restart itself programmaticaly

---
+++ RestartActor/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestartActor
+{
+    public static class Extensions
+    {
+        public static void Times( this int times, Action action)
+        {
+            for (int i = 0; i< times;i++)
+            {
+                action();
+            }
+        }
+    }
+}
---
+++ RestartActor/Program.cs
@@ -0,0 +1,63 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestartActor
+{
+    class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            var value = Context.System.Settings.LogDeadLetters;
+            Receive<string>(x => Console.WriteLine("Hello, {0}", x));
+            Receive<RestartMessage>(x => ((IInternalActorRef)Self).Restart(new Exception("Some Reason")));
+
+            Receive<RestartMessageDoNotUseThisApproach>(x =>
+            {
+                //Do not use this approach. All messages received after stop will be lost?
+                var internalRef = ((IInternalActorRef)Self);
+                internalRef.Stop();
+                internalRef.Start();
+            });
+        }
+        public static Props Props = Props.Create(() => new GreetingActor());
+        protected override void PreRestart(Exception reason, object message)
+        {
+            base.PreRestart(reason, message);
+        }
+    }
+
+    class RestartMessage
+    {
+
+    }
+
+    class RestartMessageDoNotUseThisApproach
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("system");
+            var greetingActor = system.ActorOf(GreetingActor.Props);
+
+            greetingActor.Tell("John");
+            greetingActor.Tell(new RestartMessage());
+
+            Thread.Sleep(1000);
+
+            20.Times(() => greetingActor.Tell("Jack"));        
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
